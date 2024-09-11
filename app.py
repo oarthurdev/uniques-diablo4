5b117203acc54c6b6947cd2e553856fbf9c1ddcd
Arthur Wagenknecht <oarthurdev@gmail.com>
2024-09-11 18:00:10 -0400

    Refactor

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from app import create_app
+from application import create_app
 
 if __name__ == '__main__':
     application = create_app()
--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,7 @@ from .config import Config
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object('application.config.Config')
     db.init_app(app)
     app.register_blueprint(main_bp)
     return app
