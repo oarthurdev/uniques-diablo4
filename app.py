1704d67e6f686ebb26cbac0682ddfb57780930ce
Arthur Wagenknecht <oarthurdev@gmail.com>
2024-09-11 10:20:10 -0400

    teste

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
