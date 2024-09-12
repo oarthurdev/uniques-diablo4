409b0d48c2ad51b7464cc96f00d580e495680c0d
Arthur Wagenknecht <oarthurdev@gmail.com>
2024-09-11 20:33:40 -0400

    Refactor

--- application/__init__.py
+++ application/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from .models import db
 from .routes import bp as main_bp
 from .config import Config
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('application.config.Config')
     db.init_app(app)
     app.register_blueprint(main_bp)
+    CORS(app)
+    app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
     return app
