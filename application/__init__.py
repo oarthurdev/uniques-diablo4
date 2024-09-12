e1747fa0a197f102b3a53af7284d1a16cdce9653
Arthur Wagenknecht <oarthurdev@gmail.com>
2024-09-12 16:36:53 -0400

    teste cookies

--- application/__init__.py
+++ application/__init__.py
@@ -10,7 +10,7 @@ def create_app():
     CORS(app, supports_credentials=True)
     app.config['JWT_SECRET_KEY'] = Config.SECRET_KEY  # Alterar para o seu segredo real
     app.config['JWT_ACCESS_COOKIE_PATH'] = '/'  # Caminho onde o cookie é acessível
-    app.config['JWT_COOKIE_SECURE'] = False  # Defina como True em produção se estiver usando HTTPS
+    app.config['JWT_COOKIE_SECURE'] = True  # Defina como True em produção se estiver usando HTTPS
     app.config['JWT_COOKIE_CSRF_PROTECT'] = True  # Se você estiver usando proteção CSRF com JWT
     app.config.from_object(Config)
 
