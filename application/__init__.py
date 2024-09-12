50597c746e42e2aa0f08ca288f4fb81e6938be9e
Arthur Wagenknecht <oarthurdev@gmail.com>
2024-09-12 17:10:42 -0400

    teste cookies

--- application/__init__.py
+++ application/__init__.py
@@ -3,14 +3,12 @@ from .models import db
 from .routes import bp as main_bp
 from .config import Config
 from flask_jwt_extended import JWTManager
-from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, supports_credentials=True)
     app.config['JWT_SECRET_KEY'] = Config.SECRET_KEY  # Alterar para o seu segredo real
     app.config['JWT_ACCESS_COOKIE_PATH'] = '/'  # Caminho onde o cookie é acessível
-    app.config['JWT_COOKIE_SECURE'] = True  # Defina como True em produção se estiver usando HTTPS
+    app.config['JWT_COOKIE_SECURE'] = False  # Defina como True em produção se estiver usando HTTPS
     app.config['JWT_COOKIE_CSRF_PROTECT'] = True  # Se você estiver usando proteção CSRF com JWT
     app.config.from_object(Config)
 
