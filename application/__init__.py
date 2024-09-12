bcb73083f2f2fe3c5c40124ee0c269bec10ab638
Arthur Wagenknecht <oarthurdev@gmail.com>
2024-09-12 17:15:56 -0400

    teste cookies

--- application/__init__.py
+++ application/__init__.py
@@ -7,7 +7,7 @@ from flask_jwt_extended import JWTManager
 def create_app():
     app = Flask(__name__)
     app.config['JWT_SECRET_KEY'] = Config.SECRET_KEY  # Alterar para o seu segredo real
-    app.config['SESSION_COOKIE_DOMAIN'] = '.uniques-diablo4.vercel.app'  # Configura o domínio
+    app.config['SESSION_COOKIE_DOMAIN'] = 'uniques-diablo4.vercel.app'  # Configura o domínio
     app.config['SESSION_COOKIE_PATH'] = '/'  # Configura o caminho
     app.config['SESSION_COOKIE_SECURE'] = True  # Somente para HTTPS
     app.config['SESSION_COOKIE_HTTPONLY'] = True
